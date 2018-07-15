9fc187dbcd39d45f18feccb6e403e3c04d5e0cdc
Darkniel <daniel.visualfx@gmail.com>
2018-07-15 02:44:51 -0500

    Start

---
+++ src/app/challenge/challenge.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-challenge',
+  templateUrl: './challenge.component.html',
+  styleUrls: ['./challenge.component.css']
+})
+export class ChallengeComponent implements OnInit {
+
+  constructor() {
+
+  }
+  ngOnInit() {
+  }
+  onSubmit() {
+
+  }
+
+}
